e174e8f1468b7c717231c638924edd82cc510628
pramesh-sherla <66243912+pramesh-sherla@users.noreply.github.com>
2020-06-01 15:08:40 +0530

    Update sumAvg.c

--- sumAvg.c
+++ sumAvg.c
@@ -2,21 +2,28 @@
 int main()
 {
 	int n;
+	int i,j,k;
 	printf("Enter the number of Subjecs : ");scanf("%d",&n);
 	int arr[n];
 	int *ptr;
 	ptr = arr;
-	for(int i=0;i<n;i++)
+	for(i=0;i<n;i++)
 	{
 		printf("Enter marks in %d subject : ",i+1);scanf("%d",ptr+i);
 	}
 	int sum = 0, avg = 0;
-	for(int j=0;j<n;j++)
+	for(j=0;j<n;j++)
 	{
 		sum = sum + *(ptr+j);
 	}
 	printf("Total Marks   : %d\n",sum);
 	avg=sum/n;
 	printf("Average marks : %d",avg);
+	int min = *ptr;
+	for(k =0;k,n;k++)
+	{
+		if (*(ptr+k)<min) min = *(ptr+k);
+	}
+	printf("Min marks are : %d",min);
 	return 0;
 }
